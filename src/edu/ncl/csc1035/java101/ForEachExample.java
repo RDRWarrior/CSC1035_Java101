7804c8aca485ba6406a910e78deea4e566e5650d
c2042957 <m.grady2@newcastle.ac.uk>
2023-02-15 14:10:35 +0000

    Update

--- src/edu/ncl/csc1035/java101/ForEachExample.java
+++ src/edu/ncl/csc1035/java101/ForEachExample.java
@@ -1,4 +1,20 @@
 package edu.ncl.csc1035.java101;
 
-public class ForEachExample {
-}
+public class ForEachExample
+    {
+        public static void main(String[] args)
+            {
+                String[] days = {"Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"};
+
+                for(int i = 0; i < days.length; i++)
+                    {
+                        System.out.println(days[i]);
+                    }
+
+                //Does the same but neater
+                for(String day: days)
+                    {
+                        System.out.println(day);
+                    }
+            }
+    }
