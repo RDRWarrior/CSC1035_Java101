273b7e0b1aad84f46766abadb7f955a45a61db1c
c2042957 <m.grady2@newcastle.ac.uk>
2023-02-15 13:56:23 +0000

    Update

--- src/edu/ncl/csc1035/java101/LoopExample.java
+++ src/edu/ncl/csc1035/java101/LoopExample.java
@@ -2,5 +2,29 @@ package edu.ncl.csc1035.java101;
 
 public class LoopExample
     {
+        public static void main(String[] args)
+            {
+                //For loop
+                for (int i = 2; i < 10; i+=2)
+                    {
+                        System.out.println("hello, world");
+                    }
 
+                //While loop
+                int i = 0;
+                while (i < 10)
+                    {
+                        System.out.println(i);
+                        i++;
+                    }
+
+                //Do while loop
+                int j = 0;
+                do
+                    {
+                        System.out.println(j);
+                        j++;
+                    }
+                while (j < 10);
+            }
     }
