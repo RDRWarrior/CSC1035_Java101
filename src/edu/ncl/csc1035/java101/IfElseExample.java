adbd3fdffa9e8667f3f6ab2cafc4f4c6d047c655
c2042957 <m.grady2@newcastle.ac.uk>
2023-02-15 14:17:25 +0000

    Created IfElseExample

---
+++ src/edu/ncl/csc1035/java101/IfElseExample.java
@@ -0,0 +1,16 @@
+package edu.ncl.csc1035.java101;
+
+public class IfElseExample
+    {
+        public static void main(String[] args)
+            {
+                boolean holiday = false;
+                if(holiday == true)
+                    {
+                        System.out.println("Woohoo!");
+                    }
+                    else {
+                        System.out.println("Eat. Sleep. Work. Repeat");
+                    }
+            }
+    }
