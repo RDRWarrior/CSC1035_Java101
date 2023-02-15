bf06abd7c5ee775a282eb17d731beec32371f286
c2042957 <m.grady2@newcastle.ac.uk>
2023-02-15 14:24:46 +0000

    Created SwitchExample

---
+++ src/edu/ncl/csc1035/java101/SwitchExample.java
@@ -0,0 +1,23 @@
+package edu.ncl.csc1035.java101;
+
+public class SwitchExample
+    {
+        public static void main(String[] args)
+            {
+                int input = 1;
+                switch(input)
+                    {
+                        case 1:
+                            System.out.println("A partridge in a pear tree");
+                            break;
+                        case 2:
+                            System.out.println("Two turtle doves");
+                            break;
+                        case 3:
+                            System.out.println("Three French hens");
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+            }
+    }
