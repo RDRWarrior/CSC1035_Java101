2d4cc18d21c29d21b44992ea3cde59b935877003
c2042957 <m.grady2@newcastle.ac.uk>
2023-02-15 14:49:10 +0000

    Created MethodExampleMain

---
+++ src/edu/ncl/csc1035/java101/MethodExampleMain.java
@@ -0,0 +1,10 @@
+package edu.ncl.csc1035.java101;
+
+public class MethodExampleMain {
+    public static void main(String[] args)
+        {
+            Method m = new Method();
+            int answer = m.calculate(5, 10);
+            System.out.println(answer);
+        }
+}
